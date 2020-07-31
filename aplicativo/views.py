36db3cfb96757293eb3ce3877b09b96238e47e50
Gabriel Lopes <biel_lopes@live.com>
2020-07-31 14:53:26 -0300

    mudei para CBV

--- aplicativo/views.py
+++ aplicativo/views.py
@@ -66,26 +66,6 @@ class PessoaRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
 
 
 
-# @api_view(['GET', 'PUT', 'DELETE'])
-# def pessoa_detail_change_and_delete(request, pk):
-#     try:
-#         pessoa = Pessoa.objects.get(pk = pk) #primary_key
-#     except Pessoa.DoesNotExist:
-#         return Response(status = status.HTTP_404_NOT_FOUND)
-    
-#     if request.method == 'GET':
-#         serializer = PessoaSerializer(pessoa)
-#         return Response(serializer.data)
-#     elif request.method == 'PUT':
-#         serializer = PessoaSerializer(pessoa, data = request.data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response(serializer.data)
-#         return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
-#     elif request.method == 'DELETE':
-#         pessoa.delete()
-#         return Response(status = status.HTTP_204_NO_CONTENT)
-        
 
 
     
